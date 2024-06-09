2cf0dc692b84b7f3e44a1df8bd388097a5bf83b8
Nicolas Chicaiza <jefryn@unicauca.edu.co>
2024-06-09 10:39:21 -0500

    :construction: (NWDAF) Configurando NWDAF con gin

---
+++ NFs/nwdaf/internal/util/path.go
@@ -0,0 +1,8 @@
+package util
+
+var (
+	NwdafDefaultKeyLogPath = "./log/nwdafsslkey.log"
+	NwdafDefaultPemPath    = "./config/TLS/nwdaf.pem"
+	NwdafDefaultKeyPath    = "./config/TLS/nwdaf.key"
+	NwdafDefaultConfigPath = "./config/nwdafcfg.yaml"
+)
